fbf693e5ee0e70ac1285c666af6c9f40e8805fad
Leon Ji <mgleon08@gmail.com>
2019-05-14 22:28:50 +0800

    Read file

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Provide a directory")
+		return
+	}
+
+	files, err := ioutil.ReadDir(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
